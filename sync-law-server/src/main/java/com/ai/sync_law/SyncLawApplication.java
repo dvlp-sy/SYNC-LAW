2ad87b7a68941030d483deaf7fefc45a791e7e95
dvlp-sy <sy8389b@gmail.com>
2026-07-03 18:20:38 +0900

    init: 프로젝트 초기화

---
+++ sync-law-server/src/main/java/com/ai/sync_law/SyncLawApplication.java
@@ -0,0 +1,13 @@
+package com.ai.sync_law;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncLawApplication.class, args);
+	}
+
+}
